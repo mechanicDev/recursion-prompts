39224fa49f045d90c2984ff12cedf99dd7712b2c
Jarrod <jarrod83miller@gmail.com>
2020-11-19 22:12:59 -0600

    Thru sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,82 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array[0] + sum(array.slice(1));
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  // This made my brain hurt
+  console.log(array)
+  if (Array.isArray(array[0])) {
+    console.log('Nested array')
+    console.log(array)
+    return arraySum(array[0]) + arraySum(array.slice(1));
+  } else if (array.length === 0) {
+    console.log('length 0 condition')
+    console.log(array)
+    return 0 ;
+  } else {
+    console.log('base condition')
+    console.log(array)
+    return array[0] + arraySum(array.slice(1))
+  }
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n<0) {
+    return isEven(n*-1);
+  }
+  if (n === 0) {
+    return true;
+  }
+  if (n < 4) {
+    return (n - 2 === 0);
+  }
+  if (n >= 4) {
+    if ((n - 2) === 0 || (n - 2) === 1) {
+      return true;
+    } else {
+      return isEven(n - 2);
+    }
+  }
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n < 0) {
+    return n+1 + sumBelow(n + 1);
+  } else if (n > 0) {
+    return n-1 + sumBelow(n - 1);
+  }
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
 };
 
 // 7. Compute the exponent of a number.
